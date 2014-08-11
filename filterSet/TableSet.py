e46e3dd274d4d70a834883f8ef63582d260ed43d
enricofer <enricofer@gmail.com>
2014-08-11 23:33:11 +0200

    filterSet class

---
+++ filterSet/TableSet.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+/***************************************************************************
+ postgisQueryBuilder/filterSet
+                                 A QGIS plugin
+ helps to build views in postgis
+                              -------------------
+        begin                : 2014-04-24
+        copyright            : (C) 2014 by Enrico Ferreguti
+        email                : enricofer@gmail.com
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+"""
+
+from PyQt4.QtCore import *
+from PyQt4.QtGui import *
+
+class tableSet(QtGui.QTableWidget):
+
+    def __init__(self,wg):
+        
